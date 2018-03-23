f237755a81c6adaa6116047b670127a27ff7a38f
bars <i@slavabars.ru>
2018-03-23 11:59:10 +1000

    Перевод с кириллицы на транслит по клавиатуре

--- app/src/main/java/dcns/ru/kirtolat/KirToLat.java
+++ app/src/main/java/dcns/ru/kirtolat/KirToLat.java
@@ -2,12 +2,39 @@ package dcns.ru.kirtolat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 public class KirToLat extends AppCompatActivity {
+
+    EditText text2kir, text2lat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text2kir = (EditText)findViewById(R.id.textKir);
+        text2lat = (EditText)findViewById(R.id.textLat);
+
+        text2kir.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence textKir, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence textKir, int i, int i1, int i2) {
+                text2lat.setText(textKir);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+
     }
 }
