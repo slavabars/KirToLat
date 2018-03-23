1b6523e7d5f5b9abf0f601500cb8eed950daee72
bars <i@slavabars.ru>
2018-03-23 11:38:45 +1000

    Перевод с кириллицы на транслит по клавиатуре

---
+++ app/src/main/java/dcns/ru/kirtolat/KirToLat.java
@@ -0,0 +1,13 @@
+package dcns.ru.kirtolat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KirToLat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
